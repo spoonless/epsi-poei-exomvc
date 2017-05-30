94744e7790b5d041c539c96925aa395b6e3a024b
David <dagaydevel@free.fr>
2017-05-30 17:03:00 +0200

    transformation en ejb stateless

--- src/main/java/mvc/model/AccountManager.java
+++ src/main/java/mvc/model/AccountManager.java
@@ -1,21 +1,17 @@
 package mvc.model;
 
-import javax.ejb.Lock;
-import javax.ejb.LockType;
-import javax.ejb.Singleton;
+import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 
-@Singleton
+@Stateless
 public class AccountManager {
 	
 	@PersistenceContext(unitName="accountPersistenceUnit")
 	private EntityManager entityManager;
 	
-	@Lock(LockType.WRITE)
 	public Account save(String accountName, String accountNumber, Amount amount) throws AccountAlreadyExistingException {
-		
 		try {
 			getByNumber(accountNumber);
 			throw new AccountAlreadyExistingException();
@@ -26,7 +22,6 @@ public class AccountManager {
 		}
 	}
 	
-	@Lock(LockType.READ)
 	public Account getByNumber(String accountNumber) throws AccountDoesNotExistException {
 		try {
 			return entityManager.createQuery("select a from Account a where a.number = :number", Account.class)
