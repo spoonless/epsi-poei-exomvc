d308f2fa7e8bf51b4ce855bed03d3d0f81d2b9f1
David <dagaydevel@free.fr>
2017-06-06 14:33:59 +0200

    Revert "un truc qui marche peut-être"
    This reverts commit 96ae55e9bf3c73e12732bf31228d3808d749912a.

--- src/test/java/mvc/model/AmountTest.java
+++ src/test/java/mvc/model/AmountTest.java
@@ -4,25 +4,25 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AmountTest {
-
+	
 	@Test
 	public void whenPassOneDigitAsStringFractionPart_thenDigitIsTensOfCents() {
 		Amount amount = new Amount("2", "1");
-
-		assertEquals(201, amount.getValue());
+		
+		assertEquals(210, amount.getValue());
 	}
 
 	@Test
 	public void whenPassDigitAsStringFractionPart_thenDigitIsCents() {
 		Amount amount = new Amount("2", "01");
-
+		
 		assertEquals(201, amount.getValue());
 	}
 
 	@Test
 	public void whenPassTwoDigitsAsFractionPart_thenDigitsAreTheCents() {
 		Amount amount = new Amount("2", "10");
-
+		
 		assertEquals(210, amount.getValue());
 	}
 }
